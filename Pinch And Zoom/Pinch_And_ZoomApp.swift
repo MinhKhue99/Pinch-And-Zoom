aa4946a3f7b8234872e42b3256e523b873540c48
KhuePM <minhkhuepham99@gmail.com>
2022-08-17 22:56:28 +0700

    Initial Commit

---
+++ Pinch And Zoom/Pinch_And_ZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_And_ZoomApp.swift
+//  Pinch And Zoom
+//
+//  Created by Phạm Minh Khuê on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_And_ZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

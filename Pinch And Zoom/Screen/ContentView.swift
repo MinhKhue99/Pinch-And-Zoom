31f2b9db05976ee93365e9f7ec63968b4edb3eca
KhuePM <minhkhuepham99@gmail.com>
2022-08-18 23:54:47 +0700

    Feature: Drag Gesture

--- Pinch And Zoom/Screen/ContentView.swift
+++ Pinch And Zoom/Screen/ContentView.swift
@@ -12,35 +12,54 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
 
     // MARK: - FUNCTION
-
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
 
     // MARK: - CONTENT
     var body: some View {
         NavigationView {
             ZStack {
-                // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .padding()
                     .scaleEffect(imageScale)
-                // MARK: - Scale Effect
+                // MARK: - DOUBLE TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                           resetImageState()
                         }
                     })
+                // MARK: - DRAG GESTURE
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1.0)) {
+                                imageOffset = value.translation
+                            }
+
+                        }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        }
+                    )
             }
             .navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -58,6 +77,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
     }
 }

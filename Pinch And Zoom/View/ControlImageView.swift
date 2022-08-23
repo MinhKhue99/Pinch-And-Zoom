b59395fad7eba343dbe23c3fb575df2865e0c5d8
KhuePM <minhkhuepham99@gmail.com>
2022-08-23 22:42:00 +0700

    Feature: Control Interface

---
+++ Pinch And Zoom/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch And Zoom
+//
+//  Created by Phạm Minh Khuê on 23/08/2022.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
